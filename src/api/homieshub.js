5b35428c57a6b78ec43fc0b51e7177e6a107d98a
muiezarif <36100308+muiezarif@users.noreply.github.com>
2026-01-18 06:46:52 +0500

    api url fix

--- src/api/homieshub.js
+++ src/api/homieshub.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://7e44af3754f6.ngrok-free.app/api",
-  baseURL: "http://localhost:8800/api",
-  // baseURL: "https://backend.thehomies.app/api",
+  // baseURL: "http://localhost:8800/api",
+  baseURL: "https://backend.thehomies.app/api",
   withCredentials: false, // we use Bearer tokens, no cookies needed
 });
 
